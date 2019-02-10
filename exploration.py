cd902ec7f10af066cb6678bbc96fb7d02367ddff
ChristineLong <yuelinlong@gmail.com>
2019-02-10 13:30:02 -0500

    Load the data and combine all obs together for each data source

---
+++ exploration.py
@@ -0,0 +1,25 @@
+import gzip
+import json
+import pandas as pd
+import numpy as np
+
+from pandas.io.json import json_normalize
+
+
+# Read data from a gzip compressed json to lists
+with gzip.open("obd2_trips.json.gz", "rt", encoding="utf-8") as f:
+    car_trip = json.load(f)
+
+with gzip.open("mobile_trips.json.gz", "rt", encoding="utf-8") as f:
+    cell_trip = json.load(f)
+
+# Transform the data from nested lists to pandas dataframes. Since timestamp data, no need to sort by time variable.
+def append_data(rawdata):
+    newdata = pd.DataFrame()
+
+    for i in range(len(rawdata)):
+        newdata = newdata.append(json_normalize(data = rawdata[i]))
+    return newdata
+
+cell = append_data(cell_trip)
+car = append_data(car_trip)
