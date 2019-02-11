c237ea1cbbc28a69c099fc881f72f41eadd52786
ChristineLong <yuelinlong@gmail.com>
2019-02-11 17:35:08 -0500

    Improve plot by adding axis names and stop showing the plot before save. Filter the matched trip dataset.

--- trip.py
+++ trip.py
@@ -2,6 +2,8 @@ import gzip
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib.backends.backend_pdf import PdfPages
 from pandas.io.json import json_normalize
 
@@ -58,7 +60,7 @@ def trip_duration(data, a, b):
 
 # Group data by different trips
 def group_trip(data):
-    group_data = data.groupby('trip_no').agg({'trip_id': 'min', 'timestamp': 'min'})
+    group_data = data.groupby('trip_no').agg({'trip_id': 'min', 'timestamp': 'min', 'speed':'min'})
     group_data['length'] = data[['trip_no', 'timestamp']].groupby('trip_no').agg(lambda x: x.max() - x.min())
     group_data = group_data.query('length >5 ')
 
@@ -71,18 +73,23 @@ group_car = group_trip(car)
 group_cell = group_trip(cell)
 
 
-# search for the closed length to merge
+# Search for the closed length to merge.
+# Exact time length match is not used due to the different timestamp interval used.
 df_match = pd.merge_asof(group_car.reset_index().sort_values(by='length'),
                            group_cell.reset_index().sort_values(by='length'),
                            suffixes=['_cell', '_car'],
                            on='length',
                            direction='nearest')
 
+# Filter trip which doesn't have a 0 start speed (Mainly the first one due to trip_no). Drop speed due to redundancy.
+df_match = df_match.query('speed_cell ==0 ' and 'speed_car == 0')
+df_match = df_match.drop(['speed_cell', 'speed_car'], axis=1)
+
 # Save the matched trip data to csv
 df_match.to_csv('matched_trip.csv')
 
 # Select matched trips speed data for later plotting
-with PdfPages('match_trip.pdf') as pdf:
+with PdfPages('matched_trip.pdf') as pdf:
     for i in range(len(df_match)):
         trip_index_cell = df_match.loc[i, 'trip_no_cell']
         trip_index_car = df_match.loc[i, 'trip_no_car']
@@ -91,6 +98,8 @@ with PdfPages('match_trip.pdf') as pdf:
             plt.plot(
                 cell.query('trip_no == @trip_index_cell')['timestamp'],
                 cell.query('trip_no == @trip_index_cell')['speed'])
+            plt.ylabel('Speed')
+            plt.ylabel('Timestamp: (epoch) seconds')
             filename = 'matched_trip_' + str(i)
             plt.title(filename)
             pdf.savefig()  # saves the current figure into a pdf page
